05fb826248a6e2caf24ec98cb41518a2dad2f04a
Ajay-S-Biradar <sbmunnu@gmail.com>
2023-08-09 17:50:05 +0530

    ajay

---
+++ css/main.js
@@ -0,0 +1,21 @@
+fetch('/api/data') 
+    .then(res=>res.json())
+    .then((data)=> {
+        console.log(data)
+        process(data) 
+})
+
+function process(data){
+    if(data==false){
+
+    }
+    else{
+        console.log(data)
+        user = document.querySelector('.btn')
+        console.log(user)
+        user.classList.add("hide")
+        name_of_user = document.querySelector('.user_name')
+        console.log(name_of_user)
+        name_of_user.innerText = data
+    }
+}
